d62a8d53eb774e6e07532e9a60d5a509a30a43de
Christian <emicarmo@hotmail.com>
2023-07-20 11:58:36 -0300

    web responsive

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* Funciones para abrir y cerrar menu hamburguesa */
+
+function abrir() {
+
+    document.getElementById('desplegable').style.display = 'flex'
+
+}
+
+
+function cerrar() {
+
+    document.getElementById('desplegable').style.display = 'none'
+
+}
+
+/* Funciones para abrir y cerrar el modal mail */
+
+function abrirModal() {
+
+    document.getElementById('modal').style.display = 'flex'
+
+}
+
+
+function cerrarModal() {
+
+    document.getElementById('modal').style.display = 'none'
+
+}
